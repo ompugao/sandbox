245800b40e08b79df8fd085120d8a037212d4ecc
Shohei Fujii <fujii.shohei@gmail.com>
2019-04-05 19:01:12 +0900

    split nonlinearmpc to seperate files

---
+++ nonlinear_mpc_cpp_mecanum_wheel/nonlinearmpc.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "vehicle.h"
+#include "obstacle.h"
+#include <memory>
+#include <Eigen/Dense>
+
+class NonLinearMPC {
+public:
+  NonLinearMPC(const Vehicle& v, const std::shared_ptr<Parameters> params);
+
+  virtual ~NonLinearMPC() {}
+
+  Eigen::MatrixXd compute_input(const double t /*[s] */, const Eigen::MatrixXd& xref, std::vector<Obstacle*>& obstacles);
+
+  Eigen::MatrixXd predict_adjoint_variables(const Eigen::MatrixXd& states, const Eigen::MatrixXd& xref, const std::vector<Obstacle*>& obstacles, const Eigen::MatrixXd& U, double dt);
+  Eigen::VectorXd compute_previous_lambda(const Eigen::VectorXd& state, const Eigen::VectorXd& state_ref,
+                                          const Eigen::VectorXd& lambda, const std::vector<Obstacle*>& obstacles,
+                                          const Eigen::VectorXd& _u, double dt);
+
+  Eigen::MatrixXd compute_F(const Eigen::MatrixXd& states, const Eigen::MatrixXd& lambdas, const Eigen::MatrixXd& U);
+
+  const Vehicle* vehicle_;
+  const int num_constraints_ = 5;
+  const int num_dummy_inputs_ = 5;  // == num_constraints_; ?
+  std::shared_ptr<Parameters> params_;
+
+  Eigen::MatrixXd U_;
+  //Eigen::MatrixXd lambdas_;  // (i, ht*j)
+  // Eigen::VectorXd mu_;
+};
+
